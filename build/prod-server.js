4c4653100d78d77f656e8e8bfc2630b5f152fb55
Anna Thomas <annaet@uk.ibm.com>
2017-07-18 14:43:12 +0100

    Add cfenv

--- build/prod-server.js
+++ build/prod-server.js
@@ -2,15 +2,17 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var bodyParser = require('body-parser')
+var cfenv = require('cfenv')
 
 var app = express()
+var appEnv = cfenv.getAppEnv()
 
 app.use(bodyParser.json())
 app.use(serveStatic(path.join(__dirname, '../dist')))
 
 require('../server/routes')(app)
 
-var port = process.env.PORT || 3000
+var port = appEnv.port || process.env.PORT || 3000
 
 app.listen(port)
 console.log('server started ' + port)
