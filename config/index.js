f6de4e0abd69f8a071007e76c58c45b88bbe9f89
Anna Thomas <annaet@uk.ibm.com>
2017-07-19 10:40:02 +0100

    Update CE Store url again

--- config/index.js
+++ config/index.js
@@ -42,7 +42,7 @@ module.exports = {
     cssSourceMap: false
   },
   ceStore: {
-    url: 'ce-store-20170719092855479.eu-gb.mybluemix.net',
+    url: 'ce-store-conv.eu-gb.mybluemix.net',
     store: 'DEFAULT'
   }
 }
